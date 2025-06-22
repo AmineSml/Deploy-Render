5418ce4080b2fb730bd7ad2f35a62f1ae97df9d5
AmineSml <m.aminesmaili@gmail.com>
2025-06-23 00:23:05 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { chromium } from 'playwright';
 import cors from 'cors';
 
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
